946532a2a59a25adefd8dfdc2c8d2457d565fe6e
oreotamish <sinhatamish@gmail.com>
2024-02-06 00:50:30 +0530

    Spacy implementation and pipeline initialization

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from typing import Union
 from uvicorn.workers import UvicornWorker
 from fastapi import FastAPI
-from case_tokenization import segment
+# from case_tokenization import segment
+import spacy
+import pandas as pd
+import boto3
+from io import StringIO
+import datetime
+import os
+# import dotenv
+
+# dotenv.load_dotenv('../.env')
 
 app = FastAPI()
 
@@ -18,3 +27,67 @@ def segment(date: str):
         return {"message": f"Data processing completed for {date}"}
     except Exception as e:
         return {"message": f"An error occurred: {str(e)}"}, 500
+
+
+
+def segment(date: str):
+    nlp = spacy.load("en_core_web_sm")
+
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
+
+    s3_view = boto3.resource(
+        "s3",
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
+
+    s3_bucket = "digital-adhivakta"
+    prefix = "Cases/date="
+
+    default = (datetime.datetime.now() - datetime.timedelta(days=3)).strftime("%Y%m%d")
+    date_formatted = datetime.datetime.strptime(date or default, "%Y%m%d").strftime(
+        "%Y%m%d"
+    )
+
+    prefix = prefix + date_formatted
+    response = s3.list_objects_v2(Bucket=s3_bucket, Prefix=prefix)
+
+    files = response.get("Contents", [])
+    cases = []
+    for file in files:
+        file_key = file.get("Key")
+        if file_key.endswith(".txt"):
+            cases.append(file_key)
+
+    case_data = []
+
+    for case in cases:
+        case_content = (
+            s3_view.Object(s3_bucket, case).get()["Body"].read().decode("utf-8")
+        )
+        doc = nlp(case_content)
+
+        sentences = [sent.text for sent in doc.sents]
+
+        case_data.append(
+            {
+                "case_no": case,
+                "datedate_of_judgement": date_formatted,
+                "tokens": sentences,
+            }
+        )
+
+    case_df = pd.DataFrame(case_data)
+    case_buffer = StringIO()
+    case_csv = case_df.to_csv(case_buffer, index=False)
+    case_buffer.seek(0)
+
+    s3.put_object(
+        Body=case_buffer.getvalue(),
+        Bucket=s3_bucket,
+        Key=f"Tokens/date_{date_formatted}.csv",
+    )
